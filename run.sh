32e24f16334cde79b6d4929f695f27a7aa15a00f
Justin Lopato <justin@lopato.org>
2023-11-08 15:16:26 -0600

    first commit

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install Java
+sudo apt update
+sudo apt upgrade
+sudo apt install openjdk-19-jre
+
+# Download server.jar
+wget https://download.getbukkit.org/craftbukkit/craftbukkit-1.20.2.jar
+mv craftbukkit-1.20.2.jar server.jar
+
+# Agree to eula
+echo "eula=true" > eula.txt
+
+# Download plugins
+mkdir plugins
+cd plugins
+wget -0 essentialsX.jar https://cdn.modrinth.com/data/hXiIvTyT/versions/puUfqBpY/EssentialsX-2.20.1.jar 
+wget -0 worldedit.jar https://dev.bukkit.org/projects/worldedit/files/4793142/download
+cd ..
+
+# Run server
+java -jar server.jar
