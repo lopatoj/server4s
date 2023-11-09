cde1de7ccba302d1e309c46c1fad3cb8d3d3d1a2
Justin Lopato <justin@lopato.org>
2023-11-09 13:26:18 -0600

    jhdjkdjksv

--- run.sh
+++ run.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+# Go to home directory
+cd ~
+
 # Install Java
 sudo apt update
 sudo apt upgrade
 sudo apt install openjdk-19-jre
 
 # Download server.jar
-wget https://download.getbukkit.org/craftbukkit/craftbukkit-1.20.2.jar
-mv craftbukkit-1.20.2.jar server.jar
+wget -O server.jar https://download.getbukkit.org/craftbukkit/craftbukkit-1.20.2.jar
 
 # Agree to eula
 echo "eula=true" > eula.txt
 
 # Download plugins
 mkdir plugins
-cd plugins
-wget -0 essentialsX.jar https://cdn.modrinth.com/data/hXiIvTyT/versions/puUfqBpY/EssentialsX-2.20.1.jar 
-wget -0 worldedit.jar https://dev.bukkit.org/projects/worldedit/files/4793142/download
-cd ..
+wget -O plugins/essentialsX.jar https://cdn.modrinth.com/data/hXiIvTyT/versions/puUfqBpY/EssentialsX-2.20.1.jar 
+wget -O plugins/worldedit.jar https://dev.bukkit.org/projects/worldedit/files/4793142/download
+
 
 # Download server.properties
-wget -0 server.properties https://raw.githubusercontent.com/lopatoj/server4s/main/server.properties
+wget -O server.properties https://raw.githubusercontent.com/lopatoj/server4s/main/server.properties
 
 # Run server
+cd ~
 java -jar server.jar
